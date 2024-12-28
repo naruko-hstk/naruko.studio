ffceb2d795689df6a02c7c23e91817afb1ec4eff
星月なるこ <projectcsj@gmail.com>
2024-12-29 05:43:43 +0800

    feat: Add link to home
    Make logo can link to home page

--- src/components/header/logo.tsx
+++ src/components/header/logo.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image"
+import Link from "next/link"
 
 export default function Logo() {
   return (
     <>
-      <Image
-        src="/vercel.svg"
-        alt="なるこStudio"
-        width={16}
-        height={16}
-        className="float-left"
-      />
-      <span className="ml-2">なるこStudio</span>
+      <Link href="/">
+        <Image
+          src="/vercel.svg"
+          alt="なるこStudio"
+          width={16}
+          height={16}
+          className="float-left"
+        />
+        <span className="ml-2">なるこStudio</span>
+      </Link>
     </>
   )
 }
